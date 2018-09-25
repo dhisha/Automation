39d79c5e9d7f64b8a392a0e2057e849b9c064a8f
dhisha <43537385+dhisha@users.noreply.github.com>
2018-09-25 11:49:55 +0530

    Initial commit

---
+++ src/qsp/DemoA.java
@@ -0,0 +1,19 @@
+package qsp;
+
+import java.io.File;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class DemoA {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		Workbook w=WorkbookFactory.create(new File("./data/booknew.xlsx"));
+		String s=w.getSheet("sheet1").getRow(0).getCell(0).toString();
+		System.out.println(s);
+		
+
+	}
+
+}
